00e8ed40ba085643138d2a5024425f90f586786c
Simon1511 <simon2002.schoenmackers@gmail.com>
2025-08-05 06:33:25 +0300

    sm8350-common: Build samsung keymaster
    Change-Id: Ibab06aa84f3f5ce32c27b4c20a0da02907518f93

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,6 +87,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --board $(BOARD_NAME)
 BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
 
+# Keymaster
+TARGET_KEYMASTER_VARIANT := samsung
+
 # Additional root folders
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
